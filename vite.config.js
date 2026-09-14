378e6baf461a2cbf4248986c3d409f6fbebbe048
Josh <9404733+FlirtyMomma@users.noreply.github.com>
2026-09-14 20:34:48 +0100

    fix: explicit PWA any and maskable icons for Windows Desktop compatibility

--- vite.config.js
+++ vite.config.js
@@ -20,7 +20,13 @@ export default defineConfig({
         icons: [
           {
             src: 'icon.png',
-            sizes: '192x192 512x512',
+            sizes: '192x192',
+            type: 'image/png',
+            purpose: 'any'
+          },
+          {
+            src: 'icon.png',
+            sizes: '512x512',
             type: 'image/png',
             purpose: 'any maskable'
           }
